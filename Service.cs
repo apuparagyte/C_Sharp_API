0e8ab7f8261faf83df82e28ef091f2f93c056b16
Apurva Gajbhiye <Apurva.Gajbhiye@phreesia.com>
2024-02-19 23:26:52 +0530

    Initial Code

---
+++ Service.cs
@@ -0,0 +1,65 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFakeAPI
+{
+    public class Service
+    {
+        string baseUrl = "https://the-one-api.dev/v2/";
+        string authToken = "Bearer G4Dkrni57v_Li_VpazY0";
+
+
+        public RestResponse GETlistofmovies()
+        {
+            RestClient client = new RestClient(baseUrl);
+
+            RestRequest request = new RestRequest("movie",Method.Get);
+
+            request.AddHeader("Authorization", "Bearer G4Dkrni57v_Li_VpazY0");
+
+            RestResponse response = new RestResponse();
+
+            response =client.Execute(request);
+
+            return response;
+
+        }
+
+        public RestResponse GETspecificmovies()
+        {
+            RestClient client = new RestClient(baseUrl);
+
+            RestRequest requestmovie = new RestRequest("movie/5cd95395de30eff6ebccde56", Method.Get);
+
+            requestmovie.AddHeader("Authorization", "Bearer G4Dkrni57v_Li_VpazY0");
+
+            RestResponse response = new RestResponse();
+
+            response = client.Execute(requestmovie);
+
+            return response;
+
+        }
+        public RestResponse GETHobbitSeries()
+        {
+            RestClient client = new RestClient(baseUrl);
+
+            RestRequest requestmovie = new RestRequest("movie/5cd95395de30eff6ebccde57", Method.Get);
+
+            requestmovie.AddHeader("Authorization", "Bearer G4Dkrni57v_Li_VpazY0");
+
+            RestResponse response = new RestResponse();
+
+            response = client.Execute(requestmovie);
+
+            return response;
+
+        }
+
+    } 
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using RestSharp;
+using System.Net;
+
+namespace DemoFakeAPI
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetListofMovies()
+        {
+            Service service = new Service();
+            RestResponse response = service.GETlistofmovies();
+            Assert.IsNotNull(response);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void GetSpecificMovie()
+        {
+            Service service = new Service();
+            RestResponse response = service.GETspecificmovies();
+            Assert.IsNotNull(response);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void GetHobbitSeries()
+        {
+            Service service = new Service();
+            RestResponse response = service.GETHobbitSeries();
+            Assert.IsNotNull(response);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
